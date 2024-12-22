8ce2f2d59ba0fce233142e13641e2432c4b9bacb
Yujia Gu <yujiagu1010@gmail.com>
2024-12-22 17:15:55 +1030

    Create user token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js"
 
 const createUser = asyncHandler(async (req,res) => {
   const {username, email,password} = req.body;
@@ -23,6 +24,7 @@ const createUser = asyncHandler(async (req,res) => {
 
   try {
     await newUser.save();
+    createToken(res, newUser._id);
 
     res.status(201)
     .json({
