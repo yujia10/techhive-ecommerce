179f131d43efdb8c030e4cd34ed33c0467182d7c
Yujia Gu <yujiagu1010@gmail.com>
2024-12-22 17:01:53 +1030

    Add basic validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,40 @@
-import User from '../models/userModel.js'
-import asyncHandler from '../middlewares/asyncHandler.js'
+import User from '../models/userModel.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
+import bcrypt from "bcryptjs";
 
 const createUser = asyncHandler(async (req,res) => {
-  const {username, email,password} = req.body
+  const {username, email,password} = req.body;
 
+  // Check if all the required fields are filled
+  if(!username || !email || !password) {
+    throw new Error("Please fill all the inputs.");
+  }
+
+  // Check if user email exists
+  const userExists = await User.findOne({email});
+  if (userExists) res.status(400).send("User already exists");
+
+  // Encrypt password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password,salt)
+
+  // Create  a new user
+  const newUser = new User({username, email, password: hashedPassword});
+
+  try {
+    await newUser.save();
+
+    res.status(201)
+    .json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 export { createUser };
