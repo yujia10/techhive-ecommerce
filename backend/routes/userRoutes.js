4740373500126ff6330df569f0b03a7b8036495c
Yujia Gu <yujiagu1010@gmail.com>
2024-12-24 20:37:52 +1030

    Add authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {createUser, loginUser, logoutCurrentUser} from '../controllers/userController.js'
+import {createUser, loginUser, logoutCurrentUser} from '../controllers/userController.js';
+import {authenticate, authorizeAdmin} from '../controllers/userController.js';
+
 
 const router = express.Router()
-router.route('/').post(createUser);
+router.route('/').post(createUser).get(authenticate, authorizeAdmin);
 router.post('/auth',loginUser);
 router.post('/logout', logoutCurrentUser)
 
