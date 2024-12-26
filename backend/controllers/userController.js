71cef9ab1501d81d77eecf955ef01c54f182803f
Yujia Gu <yujiagu1010@gmail.com>
2024-12-26 22:00:36 +1030

    Add functionality for admins to get user profile by id

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -141,6 +141,17 @@ const deleteUserById = asyncHandler (async(req,res) => {
   }
 });
 
+const getUserById = asyncHandler (async(req,res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if(user){
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
 export {
   createUser,
   loginUser,
@@ -148,5 +159,6 @@ export {
   getAllUsers,
   getCurrentUserProfile,
   updateCurrentUserProfile,
-  deleteUserById
+  deleteUserById,
+  getUserById
  };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,8 @@ import {
   getAllUsers,
   getCurrentUserProfile,
   updateCurrentUserProfile,
-  deleteUserById
+  deleteUserById,
+  getUserById
 } from '../controllers/userController.js';
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js';
 
@@ -15,10 +16,11 @@ const router = express.Router()
 
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth',loginUser);
-router.post('/logout', logoutCurrentUser)
+router.post('/logout', logoutCurrentUser);
 router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 // Admin Routes
-router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById).get(authenticate,
+  authorizeAdmin, getUserById);
 
 export default router;
