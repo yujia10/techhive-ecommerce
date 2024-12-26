6754d364f406e89cf1f8c5eb4302dd9c98890d0f
Yujia Gu <yujiagu1010@gmail.com>
2024-12-26 21:19:02 +1030

    Add updating current user profile feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import {createUser, loginUser, logoutCurrentUser, getAllUsers} from '../controllers/userController.js';
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile
+} from '../controllers/userController.js';
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js';
 
 
@@ -7,5 +14,6 @@ const router = express.Router()
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth',loginUser);
 router.post('/logout', logoutCurrentUser)
+router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 export default router;
