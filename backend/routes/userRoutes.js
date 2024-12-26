8657a73db671acef7482d94842f77612876d02f6
Yujia Gu <yujiagu1010@gmail.com>
2024-12-26 21:45:42 +1030

    Add functionality for admins to delete user profiles

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,15 +5,20 @@ import {
   logoutCurrentUser,
   getAllUsers,
   getCurrentUserProfile,
-  updateCurrentUserProfile
+  updateCurrentUserProfile,
+  deleteUserById
 } from '../controllers/userController.js';
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router()
+
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth',loginUser);
 router.post('/logout', logoutCurrentUser)
 router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+// Admin Routes
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
 
 export default router;
