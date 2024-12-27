ab309b56468f103fd70027206bebbafe9ac69a75
Yujia Gu <yujiagu1010@gmail.com>
2024-12-27 17:44:36 +1030

    Add functionality for admins to update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -152,6 +152,29 @@ const getUserById = asyncHandler (async(req,res) => {
   }
 });
 
+const  updateUserById = asyncHandler (async(req,res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    // Admins are not supposed to change user's password but can change thier admin status.
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+})
+
 export {
   createUser,
   loginUser,
@@ -160,5 +183,6 @@ export {
   getCurrentUserProfile,
   updateCurrentUserProfile,
   deleteUserById,
-  getUserById
+  getUserById,
+  updateUserById
  };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,20 +7,29 @@ import {
   getCurrentUserProfile,
   updateCurrentUserProfile,
   deleteUserById,
-  getUserById
+  getUserById,
+  updateUserById
 } from '../controllers/userController.js';
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router()
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.route('/').post(createUser)
+.get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post('/auth',loginUser);
 router.post('/logout', logoutCurrentUser);
-router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+router.route('/profile')
+.get(authenticate,getCurrentUserProfile)
+.put(authenticate, updateCurrentUserProfile);
 
 // Admin Routes
-router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById).get(authenticate,
-  authorizeAdmin, getUserById);
+router
+.route('/:id')
+.delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate,authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
