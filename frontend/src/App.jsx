bf5d1c65eb811a24ca0a4f3f10ece882dce4ef67
Yujia Gu <yujiagu1010@gmail.com>
2024-12-29 16:41:11 +1030

    Add basic structure and styles for navigation module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import {Outlet} from 'react-router-dom';
+import Navigation from './pages/Auth/Navigation';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
