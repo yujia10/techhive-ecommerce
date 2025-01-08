bda1de9d27dd028ff977f24f14783c1acea30528
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-01-08 22:49:36 +1030

    Update category endpoints for CRUD operations

--- frontend/src/redux/api/categoryApiSlice.js
+++ frontend/src/redux/api/categoryApiSlice.js
@@ -8,7 +8,34 @@ export const categoryApiSlice = apiSlice.injectEndpoints({
         url: `${CATEGORY_URL}`,
         method: "POST",
         body: newCategory,
-      })
-    })
-  })
-})
+      }),
+    }),
+
+    updateCategory: builder.mutation({
+      query: ({ categoryId, updatedCategory }) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "PUT",
+        body: updatedCategory,
+      }),
+    }),
+
+    deleteCategory: builder.mutation({
+      query: (categoryId) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "DELETE",
+      }),
+    }),
+
+    fetchCategory: builder.query({
+      query: () => `${CATEGORY_URL}/categories`,
+    }),
+
+  }),
+});
+
+export const {
+  useCreateCategoryMutation,
+  useUpdateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoryQuery,
+} = categoryApiSlice;
