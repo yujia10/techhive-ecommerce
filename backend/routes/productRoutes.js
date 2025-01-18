89724142d8e9b83d65c2c80cc556f5e3d613fa3a
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-01-18 20:24:14 +1030

    Add fetch products function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,13 +7,18 @@ import {
   addProduct,
   updateProductDetails,
   removeProduct,
+  fetchProducts,
 } from '../controllers/productController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 import checkId from '../middlewares/checkId.js';
 
-router.route('/').post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route('/')
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
 router
   .route('/:id')
   .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
   .delete(authenticate, authorizeAdmin, removeProduct);
+
 export default router;
