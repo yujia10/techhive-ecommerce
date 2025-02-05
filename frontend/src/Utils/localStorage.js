bfbac50228bdd46164c1b92321d1ffab97fed4a6
Yujia Gu <yujiagu1010@gmail.com>
2025-02-05 16:20:49 +1030

    Add localStorage support for favourites

---
+++ frontend/src/Utils/localStorage.js
@@ -0,0 +1,24 @@
+// Add a product to a localStorage
+export const addFavouriteToLocalStorage = (product) => {
+  const favourites = getFavouritesFromLocalStorage();
+  if (!favourites.some((p) => p._id === product._id)) {
+    favourites.push(product);
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  }
+};
+
+// Remove  product from a localStorage
+export const removeFavouriteFromLocalStorage = (productId) => {
+  const favourites = getFavouritesFromLocalStorage();
+  const updateFavourites = favourites.filter(
+    (product) => product._id !== productId
+  );
+
+  localStorage.setItem("favourites", JSON.stringify(updateFavourites));
+};
+
+// Retrive favourites from a localStorage
+export const getFavouritesFromLocalStorage = () => {
+  const favouritesJSON = localStorage.getItem("favourites");
+  return favouritesJSON ? JSON.parse(favouritesJSON) : [];
+};
