e10ccd7ffda84d3b683a23821cd1df48211fd05d
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-02-12 21:39:52 +1030

    Rename the cart.js to cartUtils.js

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { updateCart } from '../../../Utils/cart';
+import { updateCart } from '../../../Utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
 	? JSON.parse(localStorage.getItem('cart'))
