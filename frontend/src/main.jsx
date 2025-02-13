0891ae20299f2ec491e577bc495191c3b1180c37
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-02-13 23:40:32 +1030

    Add items list with quantity selector and delete button

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,12 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
 import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import store from './redux/store.js';
-import UserList from './pages/Admin/UserList.jsx';
-import CategoryList from './pages/Admin/CategoryList.jsx';
-import ProductList from './pages/Admin/productList.jsx';
-import AllProducts from './pages/Admin/AllProducts.jsx';
-import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
+
 // Private Route
 import PrivateRoute from './components/PrivateRoute.jsx';
 
@@ -17,43 +14,51 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 import Login from './pages/Auth/Login.jsx';
 import Register from './pages/Auth/Register.jsx';
 
+import UserList from './pages/Admin/UserList.jsx';
 import Profile from './pages/User/Profile.jsx';
 import AdminRoute from './pages/Admin/AdminRoute.jsx';
-import Product from '../../backend/models/productModel';
+
+import CategoryList from './pages/Admin/CategoryList.jsx';
+
+import ProductList from './pages/Admin/productList.jsx';
+import AllProducts from './pages/Admin/AllProducts.jsx';
+import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 
 import Home from './pages/Home.jsx';
 import Favourites from './pages/Products/Favourites.jsx';
 import ProductDetails from './pages/Products/ProductDetails.jsx';
 
+import Cart from './pages/Cart.jsx';
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route index={true} path="/" element={<Home />} />
-      <Route path="/favourite" element={<Favourites />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route index={true} path="/" element={<Home />} />
+			<Route path="/favourite" element={<Favourites />} />
+			<Route path="/product/:id" element={<ProductDetails />} />
+			<Route path="/cart" element={<Cart />} />
 
-      {/* Registered users */}
-      <Route path="" element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} />
-      </Route>
+			{/* Registered users */}
+			<Route path="" element={<PrivateRoute />}>
+				<Route path="/profile" element={<Profile />} />
+			</Route>
 
-      {/* Admin Routes */}
-      <Route path="/admin" element={<AdminRoute />}>
-        <Route path="userlist" element={<UserList />} />
-        <Route path="categorylist" element={<CategoryList />} />
-        <Route path="productlist" element={<ProductList />} />
-        <Route path="productlist/:pageNumber" element={<ProductList />} />
-        <Route path="allproductslist" element={<AllProducts />} />
-        <Route path="product/update/:_id" element={<ProductUpdate />} />
-      </Route>
-    </Route>
-  )
+			{/* Admin Routes */}
+			<Route path="/admin" element={<AdminRoute />}>
+				<Route path="userlist" element={<UserList />} />
+				<Route path="categorylist" element={<CategoryList />} />
+				<Route path="productlist" element={<ProductList />} />
+				<Route path="productlist/:pageNumber" element={<ProductList />} />
+				<Route path="allproductslist" element={<AllProducts />} />
+				<Route path="product/update/:_id" element={<ProductUpdate />} />
+			</Route>
+		</Route>
+	)
 );
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
 );
---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,112 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaTrash } from 'react-icons/fa';
+import { addToCart, removeFromCart } from '../redux/features/cart/cartSlice';
+
+const Cart = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+
+	// Handler to update the item quantity in cart
+	const addToCartHandler = async (product, qty) => {
+		dispatch(addToCart({ ...product, qty }));
+	};
+
+	// Handler to remove items from cart
+	const removeFromCartHandler = (id) => {
+		dispatch(removeFromCart(id));
+	};
+
+	// Handler to checkout - redirects to login if needed
+	const checkoutHandler = () => {
+		navigate('login?redirect=/shipping');
+	};
+
+	return (
+		<>
+			<div className="container flex justify-around items-start wrap mx-auto mt-8">
+				{/* Empty Cart State */}
+				{cartItems.length === 0 ? (
+					<div>
+						Your cart is empty <Link to="/shop">Go To Shop</Link>
+					</div>
+				) : (
+					<>
+						{/* Cart Content Container */}
+						<div className="flex flex-col w-[80%]">
+							<h1 className="h1 text-2xl font-semibold mb-4">Shopping Cart</h1>
+
+							{/* Cart Items List */}
+							{cartItems.map((item) => (
+								<div
+									key={item._id}
+									className="flex items-center mb-[1rem] pb-2"
+								>
+									{/* Product Image */}
+									<div className="w-[5rem] h-[5rem]">
+										<img
+											src={item.image}
+											alt={item.name}
+											className="w-full h-full object-cover
+								rounded"
+										/>
+									</div>
+
+									{/* Product Details */}
+									<div className="flex-1 ml-4">
+										{/* Name link to product */}
+										<Link
+											to={`/product/${item._id}`}
+											className="text-pink-500 font-semibold"
+										>
+											{item.name}
+										</Link>
+
+										<div className="mt-2 text-white">{item.brand}</div>
+										<div className="mt-2 text-white font-bold">
+											$ {item.price}
+										</div>
+									</div>
+
+									{/* Quantity Selector and Delete Button */}
+									<div className="w-32 flex items-center">
+										<select
+											className="w-full p-1 border rounded bg-black text-white"
+											value={item.qty}
+											onChange={(e) =>
+												addToCartHandler(item, Number(e.target.value))
+											}
+										>
+											{[...Array(item.countInStock).keys()].map((x) => (
+												<option
+													key={x + 1}
+													value={x + 1}
+													className="bg-white text-black"
+												>
+													{x + 1}
+												</option>
+											))}
+										</select>
+										<div>
+											<button
+												className=" text-red-500 ml-4"
+												onClick={() => removeFromCartHandler(item._id)}
+											>
+												<FaTrash className="ml-[1rem] mt-[.5rem]" />
+											</button>
+										</div>
+									</div>
+								</div>
+							))}
+						</div>
+					</>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default Cart;
