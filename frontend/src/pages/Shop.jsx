57c74410ca4bad535e604a6b10951f47994b6267
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-02-16 19:08:14 +1030

    Add categories checkbox list

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -14,8 +14,8 @@ const Shop = () => {
 	const { categories, productes, checked, radio } = useSelector(
 		(state) => state.shop
 	);
-	const categoriesQuery = useFetchCategoriesQuer();
-	const [priceFilter, setPriceFilter] = useSelector('');
+	const categoriesQuery = useFetchCategoriesQuery();
+	const [priceFilter, setPriceFilter] = useState('');
 
 	const filteredProductsQuery = useGetFilteredProductsQuery({ checked, radio });
 
@@ -44,7 +44,7 @@ const Shop = () => {
 				dispatch(setProducts(filteredProducts));
 			}
 		}
-	});
+	}, [checked, radio, filteredProductsQuery.data, dispatch, priceFilter]);
 
 	// Handler for filtering products by brand
 	const handleBrandClick = (brand) => {
@@ -79,7 +79,42 @@ const Shop = () => {
 		setPriceFilter(e.target.value);
 	};
 
-	return <div>SHOP</div>;
+	return (
+		<>
+			<div className="container mx-auto">
+				<div className="flex md:flex-row">
+					<div className="bg-[151515] p-3 mt-2 mb-2">
+						<h2 className="h4 tetx-center py-2 bg-black rounded-full mb-2">
+							Filter by Categories
+						</h2>
+
+						{/* Categories Checkbox List */}
+						<div className="p-5 w-[15rem]">
+							{categories?.map((c) => (
+								<div key={c._id} className="mb-2">
+									<div className="flex  items-center mr-4">
+										<input
+											type="checkbox"
+											id="red-checkbox"
+											onChange={(e) => handleCheck(e.target.checked, c._id)}
+											className="w-4 h-4 text-pink-400 bg-gray-100 border-gray-300 rounded
+											 focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+										/>
+										<label
+											htmlFor="pink-checkbox"
+											className="ml-2 font-medium text-white dark:text-gray-300"
+										>
+											{c.name}
+										</label>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Shop;
