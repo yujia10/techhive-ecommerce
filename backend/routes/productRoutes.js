2fa11f23a07b7f82983bb5df64e38c461f729b9d
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-02-16 16:07:56 +1030

    Add product filtering in controller and RTK Query, initialise shop page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,32 +4,34 @@ const router = express.Router();
 
 // Controllers
 import {
-  addProduct,
-  updateProductDetails,
-  removeProduct,
-  fetchProducts,
-  fetchProductById,
-  fetchAllProducts,
-  addProductReview,
-  fetchTopProducts,
-  fetchNewProducts,
+	addProduct,
+	updateProductDetails,
+	removeProduct,
+	fetchProducts,
+	fetchProductById,
+	fetchAllProducts,
+	addProductReview,
+	fetchTopProducts,
+	fetchNewProducts,
+	filterProducts,
 } from '../controllers/productController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 import checkId from '../middlewares/checkId.js';
 
 router
-  .route('/')
-  .get(fetchProducts)
-  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+	.route('/')
+	.get(fetchProducts)
+	.post(authenticate, authorizeAdmin, formidable(), addProduct);
 
 router.route('/allproducts').get(fetchAllProducts);
 router.route('/:id/reviews').post(authenticate, checkId, addProductReview);
 
 router.get('/top', fetchTopProducts).get('/new', fetchNewProducts);
 router
-  .route('/:id')
-  .get(fetchProductById)
-  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
-  .delete(authenticate, authorizeAdmin, removeProduct);
+	.route('/:id')
+	.get(fetchProductById)
+	.put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+	.delete(authenticate, authorizeAdmin, removeProduct);
+router.route('/filtered-products').post(filterProducts);
 
 export default router;
