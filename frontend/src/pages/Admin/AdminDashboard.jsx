472ae6781fd202e9d3d2d997bcebbdb6517926ad
Yujia Gu <yujiagu1010@gmail.com>
2025-02-18 16:01:08 +1030

    Add AdminDashboard UI layout

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -8,6 +8,8 @@ import {
 
 import { useState, useEffect } from "react";
 import Loader from "../../components/Loader";
+import OrderList from "./OrderList";
+import AdminMenu from "./AdminMenu";
 
 const AdminDashboard = () => {
   const { data: sales, isSalesLoading } = useGetTotalSalesQuery();
@@ -88,7 +90,57 @@ const AdminDashboard = () => {
   }, [salesDetail]); // Runs when salesDetail changes
 
   return (
-    <div>AdminDashboard</div>
+    <>
+      <AdminMenu />
+
+      <section className="xl:ml-[4rem] md:ml-[0rem]">
+        <div className="w-[80%] flex justify-around flex-wrap">
+          <div className="rounded-lg bg-black p-5 w-[20rem] mt-5">
+            <div className="font-bold rounded-full w-[3rem] bg-pink-500 text-center p-3">
+              $
+            </div>
+
+            <p className="mt-5">Sales</p>
+            <h1 className="text-xl font-bold">
+              $ {isSalesLoading ? <Loader /> : sales.totalSales.toFixed(2)}
+            </h1>
+          </div>
+          <div className="rounded-lg bg-black p-5 w-[20rem] mt-5">
+            <div className="font-bold rounded-full w-[3rem] bg-pink-500 text-center p-3">
+              $
+            </div>
+
+            <p className="mt-5">Customers</p>
+            <h1 className="text-xl font-bold">
+              $ {isUsersLoading ? <Loader /> : customers?.length}
+            </h1>
+          </div>
+          <div className="rounded-lg bg-black p-5 w-[20rem] mt-5">
+            <div className="font-bold rounded-full w-[3rem] bg-pink-500 text-center p-3">
+              $
+            </div>
+
+            <p className="mt-5">All Orders</p>
+            <h1 className="text-xl font-bold">
+              $ {isOrdersLoading ? <Loader /> : orders?.totalOrders}
+            </h1>
+          </div>
+        </div>
+
+        <div className="ml-[10rem] mt-[4rem]">
+          <Chart
+            options={state.options}
+            series={state.series}
+            type="bar"
+            width="70%"
+          />
+        </div>
+
+        <div className="mt-[4rem]">
+          <OrderList />
+        </div>
+      </section>
+    </>
   )
 }
 
