e02820beff08c19269f52b71c6e83040e95c4d0f
Yujia Gu <yujiagu1010@gmail.com>
2025-02-23 15:18:18 +1030

    Restructure layout with flex to fix title display issue

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -56,97 +56,99 @@ const UserList = () => {
   };
 
   return (
-    <div className="p-4">
+    <div className="ml-[10rem] flex flex-col md:flex-row">
       <AdminMenu />
-      <h1 className="text-2xl font-semibold mb-4">Users</h1>
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-          <Message variant='danger'>
-            {error?.data?.message || error.error}
-          </Message>
-        ) : (
-         <div className="flex flex-col mid:flex-row">
-          {/* <AdminMenu /> */}
-          <table className="w-full md:w-4/5 mx-auto">
-            <thead>
-              <tr>
-                <th className="px-4 py-2 text-left">ID</th>
-                <th className="px-4 py-2 text-left">NAME</th>
-                <th className="px-4 py-2 text-left">EMAIL</th>
-                <th className="px-4 py-2 text-left">ADMIN</th>
-                <th className="px-4 py-2 text-left"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map ((user) => (
-                <tr key={user._id}>
-                  <td className="px-4 py-2">{user._id}</td>
-                  <td className="px-4 py-2">
-                    {editableUserId === user._id ? (
-                      <div className="flex items-center">
-                        <input type="text" value={editableUserName}
-                        onChange={(e) => setEditableUserName(e.target.value)}
-                        className = "w-full p-2 border rounded-lg"/>
-
-                        <button onClick={()=> updateHandler(user._id)} className="ml-2 bg-blue-500
-                        text-white py-2 px-4 rounded-lg">
-                        <FaCheck />
-                        </button>
-                      </div>
-                    ) : (
-                     <div className="flex items-center">
-                      {user.username} {" "}
-                      <button onClick={()=>toggleEdit(user._id, user.username,user.email)}>
-                        <FaEdit className="ml-[1rem]"/>
-                      </button>
-                     </div>
-                     )}
-                  </td>
-                  <td className = "px-4 py-2">
-                    {editableUserId === user._id ? (
-                      <div className = "flex items-center">
-                        <input type="text" value={editableUserEmail} onChange={(e) =>
-                          setEditableUserEmail(e.target.value)} className="w-full p-2 border
-                          rounded-lg" />
-                          <button onClick={() => updateHandler(user._id)} className="ml-2
-                          bg-blue-500 text-white py-2 px-4 rounded-lg">
-                            <FaCheck/>
-                          </button>
-                      </div>
-                    ) : (
-                     <div className="flex items-center">
-                      <p>{user.email}</p>
-                      <button onClick={()=>toggleEdit(user._id, user.username,user.email)}>
-                        <FaEdit className="ml-[1rem]"/>
-                      </button>
-                     </div>
-                     )}
-                  </td>
-                  <td className="px-4 py-2">
-                    {user.isAdmin ? (
-                      <FaCheck style={{color: 'green'}}/>
-                    ) : (
-                     <FaTimes style={{color: 'red'}}/>
-                     )}
-                  </td>
-                  <td className="px-4 py-2">
-                    {!user.isAdmin && (
-                      <div className="flex">
-                        <button onClick={() => deleteHandler(user._id)}
-                        className="bg-red-600 hover:bg-red-700 text-white
-                        font-bold py-2 px-4 rounded">
-                          <FaTrash />
-                        </button>
-                      </div>
-                    )}
-                  </td>
+      <div className="md:w-3/4 p-3">
+        <h1 className="text-2xl font-semibold mb-4">Users</h1>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+            <Message variant='danger'>
+              {error?.data?.message || error.error}
+            </Message>
+          ) : (
+          <div className="flex flex-col mid:flex-row">
+            {/* <AdminMenu /> */}
+            <table className="w-full md:w-4/5 mx-auto">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 text-left">ID</th>
+                  <th className="px-4 py-2 text-left">NAME</th>
+                  <th className="px-4 py-2 text-left">EMAIL</th>
+                  <th className="px-4 py-2 text-left">ADMIN</th>
+                  <th className="px-4 py-2 text-left"></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-         </div>
-      )}
+              </thead>
+              <tbody>
+                {users.map ((user) => (
+                  <tr key={user._id}>
+                    <td className="px-4 py-2">{user._id}</td>
+                    <td className="px-4 py-2">
+                      {editableUserId === user._id ? (
+                        <div className="flex items-center">
+                          <input type="text" value={editableUserName}
+                          onChange={(e) => setEditableUserName(e.target.value)}
+                          className = "w-full p-2 border rounded-lg"/>
+
+                          <button onClick={()=> updateHandler(user._id)} className="ml-2 bg-blue-500
+                          text-white py-2 px-4 rounded-lg">
+                          <FaCheck />
+                          </button>
+                        </div>
+                      ) : (
+                      <div className="flex items-center">
+                        {user.username} {" "}
+                        <button onClick={()=>toggleEdit(user._id, user.username,user.email)}>
+                          <FaEdit className="ml-[1rem]"/>
+                        </button>
+                      </div>
+                      )}
+                    </td>
+                    <td className = "px-4 py-2">
+                      {editableUserId === user._id ? (
+                        <div className = "flex items-center">
+                          <input type="text" value={editableUserEmail} onChange={(e) =>
+                            setEditableUserEmail(e.target.value)} className="w-full p-2 border
+                            rounded-lg" />
+                            <button onClick={() => updateHandler(user._id)} className="ml-2
+                            bg-blue-500 text-white py-2 px-4 rounded-lg">
+                              <FaCheck/>
+                            </button>
+                        </div>
+                      ) : (
+                      <div className="flex items-center">
+                        <p>{user.email}</p>
+                        <button onClick={()=>toggleEdit(user._id, user.username,user.email)}>
+                          <FaEdit className="ml-[1rem]"/>
+                        </button>
+                      </div>
+                      )}
+                    </td>
+                    <td className="px-4 py-2">
+                      {user.isAdmin ? (
+                        <FaCheck style={{color: 'green'}}/>
+                      ) : (
+                      <FaTimes style={{color: 'red'}}/>
+                      )}
+                    </td>
+                    <td className="px-4 py-2">
+                      {!user.isAdmin && (
+                        <div className="flex">
+                          <button onClick={() => deleteHandler(user._id)}
+                          className="bg-red-600 hover:bg-red-700 text-white
+                          font-bold py-2 px-4 rounded">
+                            <FaTrash />
+                          </button>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
