907dd9b83d3d666dc10b699891ba934aa240817a
Yujia Gu <yujiagu1010@gmail.com>
2025-02-24 19:41:07 +1030

    Change special products section to new arrivals

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
     <header className="flex items-center px-10 bg-gray-900 text-white">
       <ProductCarousel />
       <div className="flex flex-col justify-center items-start max-w-lg ml-14">
-        <h1 className="text-4xl font-bold">Welcome to TechHive</h1>
-        <p className="text-lg mt-4 text-[#B0B0B0]">Smart Tech, Smarter Living.</p>
-        <Link to='/shop' className = "bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[4rem]">
+        <h1 className="text-[3rem] font-bold">Welcome to TechHive</h1>
+        <p className="text-[1.5rem] mt-4 text-[#B0B0B0]">Smart Tech, Smarter Living.</p>
+        <Link to='/shop' className = "bg-pink-600 font-bold rounded-lg  hover:bg-pink-800 focus:ring-4 py-2 px-10 mr-[18rem] mt-[4rem] text-[1.2rem]">
         Shop Now
         </Link>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Link,useParams } from "react-router-dom";
-import { useGetProductsQuery } from "../redux/api/productApiSlice";
+import { useParams } from "react-router-dom";
+import { useGetNewProductsQuery } from "../redux/api/productApiSlice";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -7,7 +7,7 @@ import Product from "./Products/Product";
 
 const Home = () => {
   const {keyword} = useParams();
-  const {data, isLoading, isError} = useGetProductsQuery({keyword});
+  const {data, isLoading, isError} = useGetNewProductsQuery();
 
   return (
     <>
@@ -16,24 +16,20 @@ const Home = () => {
         {isError?.data.message || isError.error}
         </Message>) : (
           <>
-            <div className="flex justify-between items-center">
-              <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
-                Special Products
+            <div className="flex justify-center">
+              <h1 className="mt-[5rem] text-[2.5rem]">
+                New Arrivals
               </h1>
-
-              <Link to='/shop' className = "bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]">
-              Shop
-              </Link>
             </div>
 
             <div>
-            <div className="flex justify-center flex-wrap mt-[2rem]">
-              {data.products.map((product) => (
-                <div key={product._id}>
-                  <Product product={product} />
-                </div>
-              ))}
-            </div>
+              <div className="flex justify-center flex-wrap mt-[2rem]">
+                {data.map((product) => (
+                  <div key={product._id}>
+                    <Product product={product} />
+                  </div>
+                ))}
+              </div>
           </div>
           </>
         )}
