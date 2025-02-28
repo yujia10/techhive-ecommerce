00135e807a7bddd7d6d6c5df6c4579fd209947df
Yujia Gu <yujiagu1010@gmail.com>
2025-02-28 11:00:19 +1030

    Add shop button to banner

--- frontend/src/pages/Products/ProductCarousel.jsx
+++ frontend/src/pages/Products/ProductCarousel.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useGetTopProductsQuery } from "../../redux/api/productApiSlice";
 import Message from "../../components/Message";
 import Slider from "react-slick";
@@ -14,39 +15,50 @@ const ProductCarousel = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 3000,
   };
 
   return (
-    <div className="mb-4 lg:block xl:block md:block">
-      {isLoading ? null : error ? (
-        <Message variant="danger">
-          {error?.data?.message || error.error}
-        </Message>
-      ) : (
-        <Slider
-          {...settings}
-          className="xl:w-[50rem]  lg:w-[50rem] md:w-[56rem] sm:w-[40rem] sm:block"
+    <div className="h-[30rem] flex flex-col justify-between items-start">
+      <div className="mt-4 ml-1">
+        {isLoading ? null : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <Slider
+            {...settings}
+            className="w-full max-w-[50rem]"
+          >
+            {products.map(
+              ({
+                image,
+                _id,
+                name,
+              }) => (
+                <div key={_id}>
+                  <img
+                    src={image}
+                    alt={name}
+                    className="w-full rounded-lg object-cover h-[25rem]"
+                  />
+                </div>
+              )
+            )}
+          </Slider>
+        )}
+      </div>
+      <div className="flex">
+        <Link
+        to="/shop"
+        className="bg-pink-600 font-bold rounded-lg py-3 px-10"
         >
-          {products.map(
-            ({
-              image,
-              _id,
-              name,
-            }) => (
-              <div key={_id}>
-                <img
-                  src={image}
-                  alt={name}
-                  className="w-full rounded-lg object-cover h-[30rem]"
-                />
-              </div>
-            )
-          )}
-        </Slider>
-      )}
+          Shop Now
+        </Link>
+      </div>
+
     </div>
   );
 };
